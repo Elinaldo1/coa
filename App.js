2c1ac916acec064dc9e4b60c398bb61bd0cfd461
Elinaldo1 <elinnaldosc@gmail.com>
2021-03-05 00:25:39 -0300

    add funcção update apk expo

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 
 import React, { useState } from 'react';
 import Routes from './src/Routes';
+import { useEffect } from 'react';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+   useEffect(() => {
+      async function updateapp(){
+         const { isAvailable } = await Updates.checkForUpdateAsync()
 
+         if (isAvailable) {
+            await Updates.fetchUpdateAsync()
+            await Updates.reloadAsync()
+         }
+      }
+      updateapp();
+   },[]);
    return(<Routes />);
 }
